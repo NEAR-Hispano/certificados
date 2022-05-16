ab90f8012f7c882b32a883c1fac66559137fa802
hrpalencia <hrpmicarelli@gmail.com>
2022-05-16 15:16:58 -0400

    lista certificados y lista de administradores

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -104,6 +104,18 @@ pub struct TokenSeriesJson {
     royalty: HashMap<AccountId, u32>
 }
 
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Certificados {
+    id: i128,
+    nombre: String,
+    certificacion: String,
+	bootcamp: String,
+    img: String,
+    reference: String,
+    minteable: bool,
+}
+
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -112,6 +124,8 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
     token_series_by_id: UnorderedMap<TokenSeriesId, TokenSeries>,
     vault_id: AccountId,
+    certificate_list: UnorderedMap<AccountId, Vec<Certificados>>,
+    admin_list: Vec<AccountId>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -163,9 +177,72 @@ impl Contract {
             token_series_by_id: UnorderedMap::new(StorageKey::TokenSeriesById),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
             vault_id: vault_id.to_string(),
+            certificate_list: UnorderedMap::new(b"s".to_vec()),
+            admin_list: vec!["nft.nearcertificate.testnet".to_string()],
         }
     }
 
+    pub fn set_admin(&mut self, user_id: AccountId) {
+        self.admin_list.iter().find(|&x| x == &env::signer_account_id()).expect("Only administrators");
+        let valid = self.admin_list.iter().find(|&x| x == &user_id);
+        if valid.is_some() {
+            env::panic(b"the user is already in the list of administrators");
+        }
+        self.admin_list.push(user_id);
+    }
+
+
+    pub fn set_certificate_list(
+        &mut self,
+        account_id: AccountId,
+        nombre: String,
+        certificacion: String,
+	    bootcamp: String,
+        img: String,
+        reference: String
+    ) -> bool {
+        let user_id = account_id.clone();
+        let verificador = self.certificate_list.get(&user_id);
+        if verificador.is_some() {
+            if verificador.unwrap().iter().find(|item| item.certificacion.trim() == certificacion.trim()).is_some() {
+                env::panic(b"ya el usuario cuenta con ese certificado");
+            } else {
+                let mut certificate = self.certificate_list.get(&user_id).expect("user not found");
+                let id: i128 = (certificate.len() + 1) as i128;
+                certificate.push(Certificados {
+                    id: id,
+                    nombre: nombre,
+                    certificacion: certificacion,
+                    bootcamp: bootcamp,
+                    img: img,
+                    reference: reference,
+                    minteable: true,
+                });
+                self.certificate_list.insert(&user_id, &certificate);
+            }
+            return true
+        } else if verificador.is_none() {
+            self.certificate_list.insert(&user_id, &vec![Certificados {
+                id: 1,
+                nombre: nombre,
+                certificacion: certificacion,
+                bootcamp: bootcamp,
+                img: img,
+                reference: reference,
+                minteable: true,
+            }]);
+            return true
+        } else {
+            return false
+        }
+    }
+
+    pub fn get_certificate_list(
+        self,
+        account_id: AccountId,
+    ) -> Vec<Certificados> {
+        self.certificate_list.get(&account_id).expect("user not found")
+    }
 
     #[payable]
     pub fn nft_series(
