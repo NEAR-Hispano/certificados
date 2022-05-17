9ca824533e4f42c7f4530470fdbd29e0ad3d7b28
Linda-Bley <lindaleyrosario@gmail.com>
2022-05-17 11:23:56 -0400

    frontend cetificados

---
+++ frontend/default.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app height="90">
+      <div class="container">
+        <v-row class="d-lg-flex justify-content-between align-center">
+          <v-col
+            class="d-none"
+          >
+            <v-app-bar-nav-icon />
+          </v-col>
+          <v-col
+            class="col-3 d-flex justify-start"
+          >
+            <v-toolbar-title>
+              <router-link
+                to="/"
+                style="text-decoration: none"
+              >
+                <v-img
+                  width="200"
+                  :src="require('../assets/img/near-hispano-logo.png')"
+                />
+              </router-link>
+            </v-toolbar-title>
+          </v-col>
+          <v-col
+            class="col-6 d-flex justify-center mt-7"
+            align-self="center"
+          >
+            <v-autocomplete
+              v-model="values"
+              :items="items"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col
+            class="col-3 d-flex justify-end"
+          >
+            <div
+              v-show="!sesion"
+              class="text-right"
+            >
+              <v-btn
+                class="ma-2"
+                rounded
+                outlined
+                large
+                @click="signIn()"
+              >
+                Conectar Wallet
+              </v-btn>
+            </div>
+            <div
+              v-show="sesion"
+              class="text-right"
+            >
+              <v-menu offset-y transition="slide-x-transition">
+                <template #activator="{ on, attrs }">
+                  <v-btn
+                    class="black--text ma-2"
+                    rounded
+                    outlined
+                    large
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    {{ accountId }}
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item @click="signOut()">
+                    <v-list-item-title>Cerrar Sesión</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import * as nearAPI from 'near-api-js'
+import { CONFIG } from '~/services/api'
+const { connect, keyStores, WalletConnection, Contract } = nearAPI
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      miniVariant: true,
+      sesion: false,
+      accountId: null,
+    }
+  },
+  mounted () {
+    this.isSigned()
+  },
+  methods: {
+    async signIn () {
+      // connect to NEAR
+      const near = await connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()))
+      // create wallet connection
+      const wallet = new WalletConnection(near)
+      wallet.requestSignIn(
+        'hrpalencia.testnet'
+      )
+    },
+    async isSigned () {
+      // connect to NEAR
+      const near = await connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()))
+      // create wallet connection
+      const wallet = new WalletConnection(near)
+      if (wallet.isSignedIn()) {
+        const CONTRACT_NAME = 'book.bookshop2.testnet'
+        const contract = new Contract(wallet.account(), CONTRACT_NAME, {
+          viewMethods: ['get_profile'],
+          sender: wallet.account()
+        })
+        await contract.get_profile({
+          user_id: wallet.getAccountId()
+        }).then((res) => {
+          this.profilex = true
+        }).catch((err) => {
+          console.log(err)
+          this.profilex = false
+        })
+        this.sesion = true
+        // returns account Id as string
+        const walletAccountId = wallet.getAccountId()
+        this.accountId = walletAccountId
+      }
+    },
+    async signOut () {
+      // connect to NEAR
+      const near = await connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()))
+      // create wallet connection
+      const wallet = new WalletConnection(near)
+      wallet.signOut()
+      this.sesion = false
+      this.$router.go()
+    }
+  }
+}
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <aside class="text-h2 font-weight-bold text-center mt-8">
+      Mis Certificados
+    </aside>
+    <v-row class="mt-5">
+      <v-col
+        v-for="item, i in itemsCertificados"
+        :key="i"
+        class="col-3"
+      >
+        <viewer>
+          <v-card class="mx-auto">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              :src="item.img"
+            />
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+              >
+                Mintear
+              </v-btn>
+              <v-btn
+                :href="item.img"
+                download
+                color="orange"
+                text
+              >
+                Descargar
+              </v-btn>
+              <!-- <a
+                :href="item.img"
+                download
+                class="btn btn-success"
+              >
+                Descargar imagen
+              </a> -->
+            </v-card-actions>
+          </v-card>
+        </viewer>
+      </v-col>
+    </v-row>
+    <v-row class="mt-10">
+      <v-col class="col-6 offset-3">
+        <aside class="text-h4 font-weight-medium text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </aside>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="col-8 offset-2 text-center">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, deleniti obcaecati sed facilis, iste, ipsa voluptates suscipit ullam dolore quos sunt culpa officia vitae error quo at in eum commodi!
+        </p>
+        <nuxt-link to="https://educacion.nearhispano.org/">
+          <v-btn
+            class="ma-2 mt-5"
+            rounded
+            outlined
+            large
+          >
+            Certificate
+          </v-btn>
+        </nuxt-link>
+        
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardDashboard',
+    data () {
+      return {
+        itemsCertificados: [
+          { img: require('../assets/img/certificado.png') },
+          { img: require('../assets/img/certificado.png') },
+          { img: require('../assets/img/certificado.png') },
+          { img: require('../assets/img/certificado.png') },
+        ],
+      }
+    },
+    methods: {
+      verNearHispano () {
+        this.$router.push('https://educacion.nearhispano.org/')
+      }
+    }
+  }
+</script>
